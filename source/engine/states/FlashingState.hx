27160e347d2a42e3712347ca6b1618f24ea6cdb2
Homura <egzozu.be1.bas@gmail.com>
2025-10-17 19:16:16 +0300

    ugh

--- source/engine/states/FlashingState.hx
+++ source/engine/states/FlashingState.hx
@@ -22,7 +22,7 @@ class FlashingState extends MusicBeatState
 		final buttonAccept:String = controls.mobileC ? 'A' : 'ENTER';
 
 		warnText = new FlxText(0, 0, FlxG.width, 'Hey, watch out!\n
-			This Mod contains some flashing lights!\n
+			This Engine contains some flashing lights!\n
 			Press $buttonAccept to disable them now or go to Options Menu.\n
 			Press $buttonBack to ignore this message.\n
 			You\'ve been warned!', 32);
