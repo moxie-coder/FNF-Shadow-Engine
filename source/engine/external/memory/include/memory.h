dbe54ef5462b0421f6959dfad77ddc580ed5b262
Homura <egzozu.be1.bas@gmail.com>
2025-11-02 14:19:24 +0300

    aaaaaaaa

--- source/engine/external/memory/include/memory.h
+++ source/engine/external/memory/include/memory.h
@@ -27,7 +27,7 @@ size_t getCurrentRSS(void)
     return (size_t)0L;
 
 #elif defined(__APPLE__) && defined(__MACH__)
-    struct task_vm_info_data_t vmInfo;
+    struct task_vm_info vmInfo;
     mach_msg_type_number_t count = TASK_VM_INFO_COUNT;
 
     if (task_info(mach_task_self(), TASK_VM_INFO,
